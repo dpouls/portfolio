73d2d2f4f867d74cd1f6f7b55f07bb4069fb6e95
Dallin <dallinpoulson@gmail.com>
2021-08-17 15:54:10 -0600

    social links updated

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -10,7 +10,32 @@ function Contact() {
         type: 'Phone',
         text: '385-200-4172',
         link: 'tel:3852004172'
-    }]
+    },
+    {
+        type: 'GitHub',
+        text: 'github.com/dpouls',
+        link: 'https://github.com/dpouls'
+    },
+    {
+        type: 'LinkedIn',
+        text: 'linkedin.com/in/dallinpoulson',
+        link: 'www.linkedin.com/in/dallinpoulson'
+    },
+    {
+        type: 'CodeWars',
+        text: 'codewars.com/users/dpouls',
+        link: 'https://www.codewars.com/users/dpouls'
+    },
+    {
+        type: 'Facebook',
+        text: 'facebook.com/dpouls/',
+        link: 'https://www.facebook.com/dpouls/'
+    },
+    {
+        type: 'Instagram',
+        text: 'instagram.com/dpouls/',
+        link: 'https://www.instagram.com/dpouls/'
+    },]
     return (
         <section id='contact' className='contact-container'>
             <h1>Contact Me</h1>
@@ -19,7 +44,7 @@ function Contact() {
                 return (
                 <div className='contact-section'>
                 <label><strong>{ci.type}</strong></label>
-                <p><a className='social-link' href={ci.link}>{ci.text}</a></p>
+                <p className='contact-text'><a target="_blank" rel="noreferrer" className='social-link' href={ci.link}>{ci.text}</a></p>
                 </div>
                 )
             })}
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
   return (
     <header className="Header">
-      <h1><span>Dallin Poulson</span></h1>
+      <h1 id='page-title'><span>Dallin Poulson</span></h1>
       <nav className="nav-container">
           <ul className='nav-links'>
               <a className='header-links' href="#about-me">About Me</a>
